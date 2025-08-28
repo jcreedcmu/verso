b3e8aa933bab048b60c1382957208fee984e9d59
Jason Reed <jcreed@gmail.com>
2025-08-28 11:14:21 -0400

    A test of intermediate scope that captures bad newline behavior

--- src/tests/TestMain.lean
+++ src/tests/TestMain.lean
@@ -29,8 +29,48 @@ def testStemmer (_ : Config) : IO Unit := do
       IO.eprintln s!"{x} --> {s} (wanted '{y}')"
     throw <| IO.userError "Stemmer tests failed"
 
-def tests := [
-  testStemmer
+def testParser (dir : System.FilePath) (fn : Lean.Parser.ParserFn) : Config → IO Unit := fun config =>
+  Verso.GoldenTest.runTests {
+    testDir := ("src/tests/parser" : System.FilePath) / dir,
+    runTest := fn.test,
+    updateExpected := config.updateExpected
+  }
+
+def testManualTeX (dir : System.FilePath) : Config → IO Unit := fun config =>
+  Verso.GoldenTest.runTests {
+    testDir := ("src/tests/manual-tex" : System.FilePath) / dir,
+    runTest := fun _s => Verso.ManualTexTest.run -- TODO(admit multiple files)
+    updateExpected := config.updateExpected
+  }
+
+open Lean.Parser in
+open Verso.Parser in
+def tests : List (Config → IO Unit) := [
+  testStemmer,
+  testParser "metadataBlock" metadataBlock,
+  testParser "val" val,
+  testParser "arg" arg,
+  testParser "args" args,
+  testParser "nameAndArgs" nameAndArgs,
+  testParser "inlineTextChar" inlineTextChar,
+  testParser "manyInlineTextChar" (asStringFn (many1Fn inlineTextChar)),
+  testParser "inline/text" text,
+  testParser "inline/emph" (emph {}),
+  testParser "inline/code" code,
+  testParser "inline/role" (role {}),
+  testParser "inline" (inline {}),
+  testParser "block/code" (codeBlock {}),
+  testParser "block/header" (header {}),
+  testParser "block/blocks" (blocks {}),
+  testParser "block/recover" (recoverBlock (block {})),
+  testParser "blocks/recover" (recoverBlock (blocks {})),
+  testParser "block/directive" (directive {}),
+  testParser "block/opener" (ignoreFn blockOpener),
+  testParser "block/ulIndicator" (lookaheadUnorderedListIndicator {} (fun type => fakeAtom s! "{repr type}")),
+  testParser "block/olIndicator" (lookaheadOrderedListIndicator {} (fun type i => fakeAtom s! "{repr type} {i}")),
+  testParser "block/" (block {}),
+  testParser "document" document,
+  testManualTeX "newlines",
 ]
 
 def getConfig (config : Config) : List String → IO Config
