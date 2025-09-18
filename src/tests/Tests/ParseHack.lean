9d88ff2da9bb4a0c60346ff762f120ae2543820b
Jason Reed <jcreed@gmail.com>
2025-09-18 16:51:16 -0400

    Golfing

--- src/tests/Tests/ParseHack.lean
+++ src/tests/Tests/ParseHack.lean
@@ -4,29 +4,15 @@ import VersoManual
 
 open Verso Genre Manual
 open Lean
-open Parser (ParserFn sepByFn manyFn blankLine atomicFn Parser nodeFn
-             takeWhileFn eatSpaces ignoreFn chFn eoiFn bolThen strFn ppGroup many)
+open Parser (ParserFn Parser)
 
-private def asStringAux  : ParserFn := fun _c s => s.pushSyntax (    mkAtom "foo" )
-
-/-- Match an arbitrary Parser and return the consumed String in a `Syntax.atom`. -/
-def asStringFn (p : ParserFn) : ParserFn := fun c s =>
-  let iniSz := s.stxStack.size
-  let s := p c s
-  if s.hasError then s
-  else asStringAux c (s.shrinkStack iniSz)
-
-def myStrFn (str : String) : ParserFn := asStringFn <| fun c s =>
-  let rec go (iter : String.Iterator) (s : Parser.ParserState) :=
-    if iter.atEnd then s
-    else
-      let ch := iter.curr
-      go iter.next <| Parser.satisfyFn (· == ch) ch.toString c s
-  let iniPos := s.pos
-  let iniSz := s.stxStack.size
-  let s := go str.iter s
-  if s.hasError then s.mkErrorAt s!"'{str}'" iniPos (some iniSz) else s
-
+def myStrFn (str : String) : ParserFn := fun c s =>
+    let rec go (iter : String.Iterator) (s : Parser.ParserState) :=
+      if iter.atEnd then s
+      else
+        let ch := iter.curr
+        go iter.next <| Parser.satisfyFn (· == ch) ch.toString c s
+    go str.iter s
 
 open Lean.Parser.Term in
 def myContents : ParserFn :=
