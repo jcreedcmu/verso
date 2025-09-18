a46878dccd45a3ca71173e7aa07fccd11bdf5dae
Jason Reed <jcreed@gmail.com>
2025-09-18 16:42:52 -0400

    More minimal

--- src/tests/Tests/ParseHack.lean
+++ src/tests/Tests/ParseHack.lean
@@ -9,8 +9,8 @@ open Parser (ParserFn sepByFn manyFn blankLine atomicFn Parser nodeFn
 
 open Lean.Parser.Term in
 def myContents : ParserFn :=
-    (Parser.sepBy (Parser.atomic Parser.termParser) "," (allowTrailingSep := true )).fn >>
-    strFn "%%%" >> ignoreFn (chFn '\n')
+    (Parser.atomic Parser.termParser).fn >> Parser.optionalFn (strFn ",") >>
+    strFn "%%\n"
 
 def myParser : Parser where
   fn := myContents
@@ -22,17 +22,10 @@ elab "#mytest" p:myParser "::::::" : command => do
   IO.println (s!"mytest output: {p}")
 
 #mytest
-identifier,
-%%%
+abc,%%
 ::::::
 
-elab "#debug" txt:str : command => do
-  IO.println (← myParser.fn.test txt.getString)
-
-#debug r#"identifier %%%
-"#
 
 #mytest
-identifier
-%%%
+abc%%
 ::::::
