c2d6a15f7e7ddb8a7fd0f1f285cd17ecdad2534b
Jason Reed <jcreed@gmail.com>
2025-11-26 14:14:28 -0500

    Lifting a bunch of TeX command to monadic

--- src/verso/Verso/Code/HighlightedToTex.lean
+++ src/verso/Verso/Code/HighlightedToTex.lean
@@ -6,8 +6,10 @@ Author: Jason Reed
 import SubVerso.Highlighting
 import Verso.Method
 import Verso.Output.TeX
+import Verso.Doc.TeX
 
 open SubVerso.Highlighting
+open Verso.Doc.TeX (escapeForVerbatim verbatimInline GenreTeX TeXT)
 open Verso.Output
 open Lean (Json ToJson FromJson Quote)
 open Std (HashMap)
@@ -91,38 +93,6 @@ def highlightToken : String → Token.Kind → TeX
 | c, .withType _ => .raw c
 | c, .unknown => .raw c
 
-/--
-Replaces characters with strings simultaneously.
--/
-def replaceChars (s : String) (replace : Char → Option String) : String :=
-  let rec loop (acc : String) (pos : String.Pos.Raw) :=
-    if pos.byteIdx ≥ s.utf8ByteSize then acc
-    else
-      have : (String.Pos.Raw.next s pos).byteIdx > pos.byteIdx :=
-        String.Pos.Raw.byteIdx_lt_byteIdx_next s pos
-      let c := pos.get s
-      let s' := match replace c with | some rs => rs | none => s!"{c}"
-      loop (acc ++ s') (pos.next s)
-    termination_by s.rawEndPos.1 - pos.1
-  loop "" 0
-
-/--
-Escapes a string in an appropriate way for uses of `\Verb` and
-`\begin{Verbatim}...\end{Verbatim}` (from package `fancyvrb`) with
-command characters `\`, `{`, and `}`.
--/
-def escapeForVerbatim (s : String) : String :=
-  replaceChars s fun
-  | '{' => some "\\symbol{123}"
-  | '|' => some "\\symbol{124}"
-  | '}' => some "\\symbol{125}"
-  | '\\' => some "\\symbol{92}"
-  | _ => none
-
-/-- info: "\\symbol{123}\\symbol{124}\\symbol{125}\\symbol{92}" -/
-#guard_msgs in
-#eval escapeForVerbatim "{|}\\"
-
 defmethod Highlighting.Token.toVerbatimTeX (t : Highlighting.Token) : Verso.Output.TeX :=
   highlightToken (escapeForVerbatim t.content) t.kind
 
@@ -143,41 +113,45 @@ defmethod Highlighted.toVerbatimTeX : Highlighted → Verso.Output.TeX
   | .point _kind _info => \TeX{"[Point]"}
   | .unparsed str => .raw (escapeForVerbatim str)
 
-def verbatim (t : Verso.Output.TeX) : Verso.Output.TeX :=
-  .seq #[.raw "\\LeanVerb|", t, .raw "|"]
+-- def verbatim (t : Verso.Output.TeX) : Verso.Output.TeX :=
+--   .seq #[.raw "\\LeanVerb|", t, .raw "|"]
 
 def verbatimBlock (t : Verso.Output.TeX) : Verso.Output.TeX :=
   .seq #[.raw "\\begin{LeanVerbatim}\n", t, .raw "\n\\end{LeanVerbatim}\n"]
 
-defmethod Highlighting.Token.toTeX (t : Highlighting.Token) : Verso.Output.TeX :=
-  verbatim (t.toVerbatimTeX)
+defmethod Highlighting.Token.toTeX [Monad m] [GenreTeX g m] (t : Highlighting.Token) :
+    TeXT g m Verso.Output.TeX :=
+  verbatimInline (t.toVerbatimTeX)
 
-defmethod Highlighted.toTeX (t : Highlighted) : Verso.Output.TeX :=
+defmethod Highlighted.toTeX [Monad m] [GenreTeX g m] (t : Highlighted) :
+    TeXT g m Verso.Output.TeX :=
   let strip := t.trimOneTrailingNl.trimOneLeadingNl
   if strip.isEmpty then
-    .empty
+    pure .empty
   else if strip.containsNewline then
-    verbatimBlock (strip.toVerbatimTeX)
+    pure <| verbatimBlock (strip.toVerbatimTeX)
   else
-    verbatim (strip.toVerbatimTeX)
+    verbatimInline (strip.toVerbatimTeX)
 
 open Verso.Output.TeX in
-defmethod Highlighted.Goal.toTeX (h : Highlighted.Goal Highlighted) : Id Verso.Output.TeX := do
+defmethod Highlighted.Goal.toTeX [Monad m] [GenreTeX g m] (h : Highlighted.Goal Highlighted) : TeXT g m Verso.Output.TeX := do
   let {name, goalPrefix, hypotheses, conclusion} := h
   let mut rows : Array TeX := #[]
   if let some n := name then
     rows := rows ++ #[\TeX{\textbf{"case"} " " \Lean{n}}]
-  let toRow (h : Highlighted.Hypothesis Highlighted) : TeX :=
-    let namesTeX := h.names.map (·.toTeX) |>.toList.intersperse (.text " ")
-    .seq #[namesTeX, .text " : ", h.typeAndVal.toTeX]
-  rows := rows ++ hypotheses.map toRow
-  rows := rows ++ #[verbatim (goalPrefix) ++ conclusion.toTeX]
-  \TeX{\begin{tabular}{"l"} \Lean{rows.map (· ++ .raw "\\\\")} \end{tabular}}
+  pure .empty
+  -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  -- let toRow (h : Highlighted.Hypothesis Highlighted) : TeX :=
+  --   let namesTeX := h.names.mapM (·.toTeX) |>.toList.intersperse (.text " ")
+  --   .seq #[namesTeX, .text " : ", h.typeAndVal.toTeX]
+  -- rows := rows ++ hypotheses.map toRow
+  -- rows := rows ++ #[verbatim (goalPrefix) ++ conclusion.toTeX]
+  -- \TeX{\begin{tabular}{"l"} \Lean{rows.map (· ++ .raw "\\\\")} \end{tabular}}
 
 def messageContentsToVerbatimTeX (h : Highlighted.MessageContents Highlighted) : Verso.Output.TeX :=
   match h with
   | .text str => str
-  | .goal g => g.toTeX -- FIXME: this doesn't seem correct?
+  | .goal g => .empty -- XXXXXXXXXXXXXXXXXXXX g.toTeX -- FIXME: this doesn't seem correct?
   | .term t => t.toVerbatimTeX
   | .trace _cls _msg _children _collapsed => .empty -- FIXME: what to render here?
   | .append mcs => .seq (mcs.map messageContentsToVerbatimTeX)
